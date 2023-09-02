94c2d42361a0a3bdbcb840b9bfbb711c4a262739
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-09-02 04:29:34 -0700

    Insert format date to project

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,6 +1,9 @@
+import { format } from "date-fns";
+
 const project = function (name) {
-  let created;
-  setDate();
+  let dateCreated;
+  let sections = [];
+
   function setName(input) {
     name = input;
   }
@@ -10,12 +13,31 @@ const project = function (name) {
   }
 
   function setDate() {
-    created = new Date().getDate();
-    console.log(created);
+    dateCreated = format(new Date(), "mm/dd/yyyy");
   }
+
+  function getDate() {
+    return dateCreated;
+  }
+
+  function addSection(name) {
+    let section = {
+      title: name,
+    };
+    sections.push(section);
+  }
+
+  setDate();
+  return { getName, setName, getDate, addSection };
 };
 
-project("Inbox");
-// create date created format maybe just start implementing date-fns
-
+const p = project("Inbox");
+console.log(p.getDate(), p.getName());
 export default project;
+
+// let day = d.getDate();
+// let month = d.getMonth();
+// let year = d.getFullYear();
+// console.log(d);
+// console.log(d.getTime());
+// console.log(month, day, year);
