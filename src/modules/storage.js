0d05182b369038d116a8e567c7cace6f356efb63
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-09-07 05:33:15 -0700

    Add conditionals and logs for Storage

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,52 +1,90 @@
 import task from "./task";
 
 const Storage = function () {
-  let storageData = {
-    projects: {
-      inbox: {
-        name: "Inbox",
-        date: "06/06/2019",
-        tasks: [],
-      },
-      school: {
-        name: "School",
-        date: "09/09/2020",
-        tasks: [],
-      },
-    },
-  };
-
-  // let storedData;
-  // let parsedData;
-
-  localStorage.setItem("storageData", JSON.stringify(storageData));
+  localStorage.clear();
   let storedData = localStorage.getItem("storageData");
-  storageData = JSON.parse(storedData);
-
-  // function updateStorage() {
-  //   localStorage.setItem("storageData", JSON.stringify(storageData));
-  //   storedData = localStorage.getItem("storageData");
-  //   parsedData = JSON.parse(storedData);
-  // }
-  // updateStorage();
-
-  function addTask(key, task) {
-    storageData.projects[key].tasks.push(task);
-    localStorage.setItem("storageData", JSON.stringify(storageData));
+  console.log("Stored Data:", storedData);
+  let storageData;
+  if (storedData) {
+    console.log("Data exist in local storage");
+    // If data exists in localStorage, parse it
+    storageData = JSON.parse(storedData);
+    console.log("Parsed Data:", storageData);
+  } else {
+    // If no data exists in localStorage, initialize with default data
+    console.log("No data in local storage. Initializing with defaults.");
+    storageData = {
+      projects: {
+        inbox: {
+          name: "Inbox",
+          date: "06/06/2019",
+          tasks: [],
+        },
+        school: {
+          name: "School",
+          date: "09/09/2020",
+          tasks: [],
+        },
+      },
+    };
   }
 
-  function deleteTask(key, task) {}
+  function addTask(key, t) {
+    const newTask = task(t.title, t.dueDate, t.description);
+    console.log("Adding Task:", newTask);
+    if (!storageData.projects[key].tasks) {
+      storageData.projects[key].tasks = [newTask];
+    } else storageData.projects[key].tasks.push(newTask);
+
+    // Update localStorage with the modified data
+    localStorage.setItem("storageData", JSON.stringify(storageData));
+    console.log("Updated Data:", storageData);
+
+    console.log("Updated Local Storage:", localStorage.getItem("storageData"));
+    console.log(
+      "updated Parsed:",
+      JSON.parse(localStorage.getItem("storageData"))
+    );
+  }
+
+  function deleteTask(key, task) {
+    // Implement your deleteTask logic here
+  }
 
   function getStorage() {
     return storageData;
   }
 
-  return { addTask, getStorage };
+  return { addTask, deleteTask, getStorage };
 };
 
 let storage = Storage();
 export default storage;
+// Create an instance of the task object
 
+storage.addTask("inbox", {
+  title: "hello",
+  dueDate: "12/12/2015",
+  description: "will it finally work? ",
+});
+
+storage.addTask("inbox", {
+  title: "hi",
+  dueDate: "12/12/2015",
+  description: "round2? ",
+});
+
+// let mainStorage = storage.getStorage();
+
+// console.log("main Storage: ", mainStorage);
+
+// mainStorage.projects.inbox.tasks.forEach((t) => {
+//   console.log("t", t);
+//   console.log("direct notation:", t.title); // Access the title property
+//   console.log("getTitle():", t.getTitle()); // Access the getTitle() method
+//   console.log("getDueDate():", t.getDueDate());
+//   console.log("getDescription():", t.getDescription());
+// });
 /*
 storageData {
     Projects: {
